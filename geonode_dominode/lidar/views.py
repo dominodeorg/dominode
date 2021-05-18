bd13af2c707c626e366d1509ffcfc799b665e0f1
Wayner Barrios <waybarrios@gmail.com>
2021-05-18 13:50:47 -0500

    setting env variable for lidar

--- geonode_dominode/lidar/views.py
+++ geonode_dominode/lidar/views.py
@@ -1,9 +1,11 @@
 from django.views.generic import TemplateView
+from django.conf import settings
 
 class LidarView(TemplateView):
     template_name = "lidar/potree/data/viewer.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['BUCKET_URL'] = settings.LIDAR_BUCKET
         context['lidar_permission'] = self.request.user.is_authenticated 
         return context
